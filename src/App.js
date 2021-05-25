b0593cf6343a0a5181665dd6ebc3fae0433b9b65
Travis Bowen <Travisbowen23@gmail.com>
2021-05-24 19:07:52 -0700

    Completed axios setup and get request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import "./styles/app.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+	const [data, setData] = useState([]);
+	const [error, setError] = useState("");
+
+	useEffect(() => {
+		// GET request
+		axios
+			.get("https://pokeapi.co/api/v2/pokemon/?limit=60")
+			.then((res) => {
+				// handle success
+				setData(res.data.results);
+				console.log(res.data.results);
+			})
+			.catch((error) => {
+				// handle error
+				setError(error);
+				console.log(error);
+			});
+
+		// Will only run once (like componentDidMount in classes)
+	}, []);
+
 	return (
 		<div className='App'>
-			<h1>Hello</h1>
+			<h1>Pokemon</h1>
 		</div>
 	);
 }
